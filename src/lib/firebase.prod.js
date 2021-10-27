71c35a9a7f343054ea0ac71c16a4a34c18fdccc5
hernawana60 <hernawana60@gmail.com>
2021-10-28 06:31:41 +0700

    database repair

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -6,17 +6,17 @@ import "firebase/auth";
 import { seedDatabase } from "../seed";
 
 const config = {
-  apiKey: "AIzaSyATkEYZLzCv0A3Ep0g4WFvsPg5TMm0UpnE",
-  authDomain: "netflix-2-212ca.firebaseapp.com",
-  projectId: "netflix-2-212ca",
-  storageBucket: "netflix-2-212ca.appspot.com",
-  messagingSenderId: "168771669738",
-  appId: "1:168771669738:web:4afdbd97e8dd5bb73f5193",
+  apiKey: "AIzaSyBiZRkNK-KjR7e6pbghSRAzn5EaeSDUzlY",
+  authDomain: "netflix-c59c6.firebaseapp.com",
+  projectId: "netflix-c59c6",
+  storageBucket: "netflix-c59c6.appspot.com",
+  messagingSenderId: "468276372253",
+  appId: "1:468276372253:web:2b1c8dc83d881db2c7b7f4",
 };
 
 const firebase = Firebase.initializeApp(config);
 // 2) when seeding the database you'll have to uncomment this!
-seedDatabase(firebase);
+// seedDatabase(firebase);
 // 3) once you have populated the database (only run once!), re-comment this so you don't get duplicate data
 
 export { firebase };
